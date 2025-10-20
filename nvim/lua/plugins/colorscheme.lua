152559f9a03474378ef6707541453ab531d5591c
Harry W <harrisonrweiss1@gmail.com>
2025-10-19 21:14:17 -0400

    resolve issues in nvim with catppuccin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,10 @@ return {
     priority = 1000,
     opts = {
       flavour = "frappe",
+      transparent_background = "true",
+      float = {
+        transparent = true,
+      },
       -- Enable integrations for all installed plugins
       integrations = {
         -- Core LazyVim plugins
@@ -28,35 +32,6 @@ return {
         grug_far = true,
         lsp_trouble = true, -- for trouble.nvim
       },
-      -- Fix: Make floating windows use the same background as normal windows
-      custom_highlights = function(colors)
-        return {
-          NormalFloat = { bg = colors.base },
-          FloatBorder = { bg = colors.base, fg = colors.surface0 },
-          FloatTitle = { bg = colors.base, fg = colors.text },
-          -- Fix Snacks explorer colors
-          SnacksPickerBorder = { bg = colors.base, fg = colors.surface0 },
-          SnacksPickerInput = { bg = colors.base, fg = colors.text },
-          SnacksPickerPrompt = { fg = colors.text },
-          SnacksWinBar = { bg = colors.base, fg = colors.text },
-          SnacksWinBarNC = { bg = colors.base, fg = colors.text },
-          -- Fix cursor colors
-          Cursor = { bg = colors.text, fg = colors.base },
-          TermCursor = { bg = colors.text, fg = colors.base },
-          TermCursorNC = { bg = colors.overlay0, fg = colors.base },
-          -- Fix directory colors
-          Directory = { fg = colors.blue },
-          -- Fix which-key colors
-          WhichKey = { fg = colors.blue },
-          WhichKeyGroup = { fg = colors.mauve },
-          WhichKeyDesc = { fg = colors.text },
-          WhichKeyFloat = { bg = colors.base },
-          WhichKeyBorder = { bg = colors.base, fg = colors.surface0 },
-          -- Fix status line colors (lualine)
-          StatusLine = { bg = colors.base },
-          StatusLineNC = { bg = colors.base },
-        }
-      end,
     },
   },
   {
